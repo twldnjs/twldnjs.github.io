d34d3815fa38b45e6913977c0ac3c53ea884054a
twldnjs <jwn032@gmail.com>
2026-02-02 18:13:00 +0900

    fix nex.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  // config options here
+  output: 'export',
+  images: { unoptimized: true },
+
+  // GitHub Pages가 보통 /<repo>/ 경로라서 production에서만 basePath 적용
+  basePath: isProd && repo ? `/${repo}` : '',
+  assetPrefix: isProd && repo ? `/${repo}/` : '',
 };
 
 export default nextConfig;
